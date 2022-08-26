e59e39f277d6240ff7ceba2a1153c1a2f1c6c979
Mina Mikhael <mina.s.mikhael@gmail.com>
2022-08-26 22:48:37 +0200

    can restrict jokes endpoint

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,31 @@
-module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../secrets");
 
-    1- On valid token in the Authorization header, call next.
+const restrict = async (req, res, next) => {
+  const token = await req.headers.authorization;
 
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
+  if (!token) {
+    return next({ status: 401, message: "token required" });
+  }
 
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
+  jwt.verify(token, JWT_SECRET, (err) => {
+    if (err) {
+      next({ status: 401, message: "token invalid" });
+    } else {
+      next();
+    }
+  });
 };
+/*
+  IMPLEMENT
+  
+  1- On valid token in the Authorization header, call next.
+  
+  2- On missing token in the Authorization header,
+  the response body should include a string exactly as follows: "token required".
+  
+  3- On invalid or expired token in the Authorization header,
+  the response body should include a string exactly as follows: "token invalid".
+  */
+
+module.exports = restrict;
